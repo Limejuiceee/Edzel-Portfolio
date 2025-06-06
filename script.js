4d305721519bdde819e65329dbd363b5b1dd59cd
Your Name <your@email.com>
2025-06-07 04:57:00 +0800

    bago

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ function toggleMenu() {
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
-}
+};
+emailjs.send("service_nj90qkk", "template_burii2g", templateParams, options)
+.then(()=> alert("email sent").catch (()=>alert("email not sent")))
