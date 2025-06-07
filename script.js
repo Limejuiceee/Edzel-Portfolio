f8fdcb0c8e6867b02658926e67b55d8d804894dc
Your Name <your@email.com>
2025-06-07 12:35:50 +0800

    bagoooo

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 };
+
+function sendMail(){
+  
+}
+  let perms = {
+    name : ducument.getElementById("name").value,
+    email : ducument.getElementById("email").value,
+    subject : ducument.getElementById("subject").value,
+    message : ducument.getElementById("message").value,
+
+}
 emailjs.send("service_nj90qkk", "template_burii2g", templateParams, options)
 .then(()=> alert("email sent").catch (()=>alert("email not sent")))
